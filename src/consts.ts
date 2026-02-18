d60027c4c486b56b92c843a8d78192b7c0f4c5a2
Miranda300-CPU <jeiel.lima.miranda@gmail.com>
2026-02-17 22:17:14 -0300

    up

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'JG <3';
+export const SITE_DESCRIPTION = 'Blog pessoal de Jeiel Miranda, onde compartilho minhas experiências, pensamentos e projetos relacionados a tecnologia, programação e desenvolvimento web. Bem-vindo ao meu espaço digital!';
---
+++ src/content/blog/Primeiro Post.md
@@ -0,0 +1,9 @@
+---
+title: 'Primeiro Post'
+description: 'Criando um blog com framework Astro'
+pubDate: '2026-02-15'
+heroImage: '../../assets/blog-placeholder-3.jpg'
+author: 'Jeiel Miranda'
+---
+
+Usando modelo pré-criado pelo framework Astro...
